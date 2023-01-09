4b540a4f85958292da71fd99fe332a9c18fc257b
EnormousDique <102379927+EnormousDique@users.noreply.github.com>
2023-01-09 23:02:25 +0300

    another update

--- src/ru/muwa/shq/levels/dev/Levelvovka1010.java
+++ src/ru/muwa/shq/levels/dev/Levelvovka1010.java
@@ -9,6 +9,7 @@ public class LevelVovka1010 extends Level {
     public LevelVovka1010 (){
         try {
             objects.add(new GarbageChute (320,428));
+            System.out.println("kiss my ass");
 
         } catch (IOException e) {
             throw new RuntimeException (e);
