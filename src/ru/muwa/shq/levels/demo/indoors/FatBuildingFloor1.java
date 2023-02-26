4822dcbc6a48cb50b19deb43634f479db137d2d0
darkm <darkmarty1912@gmail.com>
2023-02-26 21:15:51 +0300

    падик 1м фалом

--- src/ru/muwa/shq/levels/demo/indoors/FatBuildingFloor1.java
+++ src/ru/muwa/shq/levels/demo/indoors/FatBuildingFloor1.java
@@ -4,6 +4,7 @@ import ru.muwa.shq.levels.Level;
 import ru.muwa.shq.levels.demo.DemoLevel0;
 import ru.muwa.shq.objects.buildings.indoors.Stairs.ApartmentDoor;
 import ru.muwa.shq.objects.buildings.indoors.Stairs.Stairs;
+import ru.muwa.shq.objects.buildings.indoors.Stairs.TestEntrance;
 import ru.muwa.shq.objects.containers.PostBox;
 import ru.muwa.shq.zones.EnterZone;
 
@@ -21,11 +22,14 @@ public class FatBuildingFloor1 extends Level
         instance = this;
         startPosX = 0;
         startPosY = 0;
-        containers.add(new PostBox(100,100));
+       // containers.add(new PostBox(100,100));
         zones.add(new EnterZone(200,200,100,100, DemoLevel0.getInstance(), 700,1800));
-        objects.add(new Stairs(150,-100));
-        objects.add(new ApartmentDoor(10,0,0));
-        objects.add(new ApartmentDoor(80,0,0));
+
+       // objects.add(new ApartmentDoor(10,0,0));
+      //  objects.add(new ApartmentDoor(80,0,0));
+        objects.add(new TestEntrance(10,10));
+        objects.add(new Stairs(150,10));
+        objects.add(new Stairs(150,80));
 
     }
 }
