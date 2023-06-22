a509fede218c4951735f6fdf1798f5f127ebd9e7
darkm <darkmarty1912@gmail.com>
2023-06-22 21:53:14 +0300

    смержил для авто мержа

--- src/ru/muwa/shq/levels/demo/demoLevel0/buildings/building7/entrance1/L1B7P1F2.java
+++ src/ru/muwa/shq/levels/demo/demoLevel0/buildings/building7/entrance1/L1B7P1F2.java
@@ -2,8 +2,8 @@ package ru.muwa.shq.levels.demo.demoLevel0.buildings.building7.entrance1;
 
 import ru.muwa.shq.levels.Level;
 import ru.muwa.shq.levels.demo.demoLevel0.DemoLevel0;
-import ru.muwa.shq.levels.demo.demoLevel0.buildings.building6.entrance1.Stairs.Stairs2;
-import ru.muwa.shq.levels.demo.demoLevel0.buildings.building6.entrance1.TamburFloor.TamburFloor2;
+
+
 import ru.muwa.shq.minigames.Elevator;
 import ru.muwa.shq.objects.GameObject;
 import ru.muwa.shq.objects.bounds.UniversalWall;
